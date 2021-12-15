41d8202490a6bfbf703a6c40fd77173522e4d7bd
BangHyeonJun <bhj2849@naver.com>
2021-12-15 22:09:41 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import vocabsData from "./data/vocabs.json";
 
 function App() {
   return (
